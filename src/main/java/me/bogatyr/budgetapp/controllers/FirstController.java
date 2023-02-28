4731720ae9346ca59b9d765fb1cd5666bae287eb
admin <kochereva_irina@mail.ru>
2023-03-01 00:04:08 +0300

    с ошибкой

--- src/main/java/me/bogatyr/budgetapp/controllers/FirstController.java
+++ src/main/java/me/bogatyr/budgetapp/controllers/FirstController.java
@@ -1,2 +1,12 @@
-package me.bogatyr.budgetapp.controllers;public class FirstController {
+package me.bogatyr.budgetapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+   @GetMapping
+    public String helloWord (){
+        return "Hello, web!";
+    }
 }
